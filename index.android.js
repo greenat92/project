73bdd6b04e464dcfe679e63a3781855a4b660a47
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-21 14:38:26 +0100

    justifyContent

--- index.android.js
+++ index.android.js
@@ -4,12 +4,17 @@ import { AppRegistry, Text, View } from 'react-native';
 class App extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-     <View style={{flex: 1, flexDirection: 'row'}}>
-       <View style={{flex:1, backgroundColor: 'powderblue'}} />
-       <View style={{flex:2, backgroundColor: 'skyblue'}} />
-       <View style={{flex:3, backgroundColor: 'steelblue'}} />
-     </View>
+      // Try setting `justifyContent` to `center`.
+        // Try setting `flexDirection` to `row`.
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        </View>
     );
   }
 }
