b66076c4fcd84775755687d417a05df36e8f6dae
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-21 14:46:03 +0100

    use prop alignItems

--- index.android.js
+++ index.android.js
@@ -4,17 +4,19 @@ import { AppRegistry, Text, View } from 'react-native';
 class App extends Component {
   render() {
     return (
-      // Try setting `justifyContent` to `center`.
-        // Try setting `flexDirection` to `row`.
-        <View style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-        </View>
+      // Try setting `alignItems` to 'flex-start'
+     // Try setting `justifyContent` to `flex-end`.
+     // Try setting `flexDirection` to `row`.
+     <View style={{
+       flex: 1,
+       flexDirection: 'row',
+       justifyContent: 'flex-end',
+       alignItems: 'flex-end',
+     }}>
+       <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+       <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+       <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+     </View>
     );
   }
 }
