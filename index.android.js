ba3ed613f6810efd42b3317a55c7358bb34e79d1
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-22 15:04:01 +0100

    Displaying a basic button

--- index.android.js
+++ index.android.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import { AppRegistry, TextInput, Text, View } from 'react-native';
+import { AppRegistry, Alert, Button, StyleSheet, View } from 'react-native';
 
-export default class  AppPizzaTranslator extends Component {
-  constructor(props){
-    super(props);
-    this.state = {text: ""};
+export default class  ButtonBasics extends Component {
+
+  _onPressButton(){
+    Alert.alert("You taped the button!")
   }
   render() {
     return (
-     <View style={{padding: 10}}>
-       <TextInput
-         style={{height: 40}}
-         placeholder='type something here'
-         onChangeText={ (text) => this.setState({text}) }
-       />
-       <Text style={{padding: 10, fontSize: 42}}>
-         {this.state.text.split(" ").map( (word) => word && '🍕').join(' ')}
-       </Text>
-
-     </View>
+            <View style={styles.container}>
+              <View style={styles.buttonContainer}>
+                  <Button
+                    onPress={this._onPressButton}
+                    title="Click me"
+                  />
+              </View>
+              <View style={styles.buttonContainer}>
+                <Button
+                  title="Click me"
+                  onPress={this._onPressButton}
+                  color="#841584"
+                />
+              </View>
+              <View style={styles.alternativeLayoutButtonContainer}>
+                <Button
+                  title="This looks great to me"
+                  onPress={this._onPressButton}
+                />
+                <Button
+                  title="ok to me"
+                  onPress={this._onPressButton}
+                  color="#841584"
+                />
+              </View>
+            </View>
     );
   }
 }
 
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('project', () => AppPizzaTranslator);
+AppRegistry.registerComponent('project', () => ButtonBasics);
+
+const styles = StyleSheet.create({
+  container:{
+      flex: 1,
+      justifyContent: 'center'
+  },
+  buttonContainer:{
+    margin: 20
+  },
+  alternativeLayoutButtonContainer:{
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
