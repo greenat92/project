e25c16a05546ea947146e633dc20127e3897eeca
LBenzahia <lakhdar.benzahia@gmail.com>
2017-07-24 12:50:08 +0100

    react-native counter app

--- index.android.js
+++ index.android.js
@@ -1,173 +1,51 @@
 import React, { Component } from 'react';
 import {
-  Alert,
   AppRegistry,
-  Image,
-  Platform,
   StyleSheet,
   Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
-  View,
-  ScrollView
+  View
 } from 'react-native';
 
-export default class  Touchables extends Component {
+class Counter extends Component {
+      state = {count: 0}
 
-  _onPressButton(){
-    Alert.alert("You taped the button!")
-  }
-  _onLongPressButton(){
-    Alert.alert("you long-pressed the button!")
-  }
+      componentDidMount(){
+        setInterval(() => {
+          this.setState({count: this.state.count + 1})
+        }, 1000)
+      }
+      render() {
+        const {count} = this.state;
+        const {color, size} = this.props;
+        return (
+          <Text style={{color, fontSize: size}}>
+            {count}
+          </Text>
+        );
+      }
+};
+
+export default class  AppCounter extends Component {
+
   render() {
     return (
-          <ScrollView>
-            <View style={styles.container}>
-              <TouchableNativeFeedback
-                onPress={this._onPressButton}
-                background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-                </View>
-              </TouchableNativeFeedback>
+      <View style={styles.container}>
+        <Counter color={'lightblue'} size={16} />
+        <Counter color={'skyblue'} size={32} />
+        <Counter color={'steelblue'} size={80} />
+        <Counter color={'darkblue'} size={140} />
+      </View>
 
-              <TouchableHighlight onPress={this._onPressButton} underlayColor='white'>
-                <View style={styles.button}>
-                  <Text style={styles.buttonText}>TouchableHighlight</Text>
-                </View>
-              </TouchableHighlight>
-
-              <TouchableOpacity onPress={this._onPressButton}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonText}>TouchableOpacity</Text>
-                </View>
-                <View style={styles.container}>
-                  <TouchableNativeFeedback
-                    onPress={this._onPressButton}
-                    background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-                    </View>
-                  </TouchableNativeFeedback>
-
-                  <TouchableHighlight onPress={this._onPressButton} underlayColor='white'>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableHighlight</Text>
-                    </View>
-                  </TouchableHighlight>
-
-                  <TouchableOpacity onPress={this._onPressButton}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableOpacity</Text>
-                    </View>
-                  </TouchableOpacity>
-
-                  <TouchableWithoutFeedback
-                    onPress={this._onPressButton}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-                    </View>
-                  </TouchableWithoutFeedback>
-
-                  <TouchableHighlight
-                    onPress={this._onPressButton}
-                    onLongPress={this._onLongPressButton}
-                    underlayColor='white'
-                    >
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>Touchable with Long Press</Text>
-                    </View>
-                  </TouchableHighlight>
-                </View>
-                <View style={styles.container}>
-                  <TouchableNativeFeedback
-                    onPress={this._onPressButton}
-                    background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-                    </View>
-                  </TouchableNativeFeedback>
-
-                  <TouchableHighlight onPress={this._onPressButton} underlayColor='white'>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableHighlight</Text>
-                    </View>
-                  </TouchableHighlight>
-
-                  <TouchableOpacity onPress={this._onPressButton}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableOpacity</Text>
-                    </View>
-                  </TouchableOpacity>
-
-                  <TouchableWithoutFeedback
-                    onPress={this._onPressButton}>
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-                    </View>
-                  </TouchableWithoutFeedback>
-
-                  <TouchableHighlight
-                    onPress={this._onPressButton}
-                    onLongPress={this._onLongPressButton}
-                    underlayColor='white'
-                    >
-                    <View style={styles.button}>
-                      <Text style={styles.buttonText}>Touchable with Long Press</Text>
-                    </View>
-                  </TouchableHighlight>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableWithoutFeedback
-                onPress={this._onPressButton}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-                </View>
-              </TouchableWithoutFeedback>
-
-              <TouchableHighlight
-                onPress={this._onPressButton}
-                onLongPress={this._onLongPressButton}
-                underlayColor='white'
-                >
-                <View style={styles.button}>
-                  <Text style={styles.buttonText}>Touchable with Long Press</Text>
-                </View>
-              </TouchableHighlight>
-              <View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-                    <View><Image source={require('./img/favicon.png')} /></View>
-            </View>
-
-            </View>
-          </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center'
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
 });
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('project', () => Touchables);
+AppRegistry.registerComponent('project', () => AppCounter);
